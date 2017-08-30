f4f1a7dd5d1ec8515d13c5c588d34bace84db5da
tmozgach <tmozgach@sfu.ca>
2017-08-30 10:16:41 -0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@ sleep 5
 # The bug with `quast`, have to test twice!
 # brew test quast || true
 
-if brew test $(brew list) --cc=gcc-4.8 |& grep -q ': failed'; then
+if (brew test $(brew list) --cc=gcc-4.8) |& grep -q ': failed'; then
   echo "Tests failed!"
   exit 1
 else
